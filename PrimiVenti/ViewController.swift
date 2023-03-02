1c14102c00f83d174536661421da226ecc8a56db
Juan Ramirez <juanramirez@Juans-MacBook-Pro.local>
2023-03-02 16:36:22 +0100

    Create 1 to 100 list UIKit

--- PrimiVenti/ViewController.swift
+++ PrimiVenti/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  @IBOutlet var tableView: UITableView!
+  
+  var numbers = [String]()
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    for i in 1...100 {
+        numbers.append(String(i))
+    }
+    
+    tableView.delegate = self
+    tableView.dataSource = self
   }
 
 
 }
 
+extension ViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    print("you click me")
+  }
+}
+
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return numbers.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    
+    cell.textLabel?.text = numbers[indexPath.row]
+    
+    return cell
+  }
+  
+  
+}
